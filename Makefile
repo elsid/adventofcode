d55a367d01404ebbb4649340c10c7bc0e12de365
Roman Siromakha <elsid@yandex-team.ru>
2015-12-28 17:31:57 +0300

    Add solution for 14.1

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: 1_1 1_2 2_1 3_1 4_1 5_1 5_2 6_1 6_2 10_1 10_2
+all: 1_1 1_2 2_1 3_1 4_1 5_1 5_2 6_1 6_2 10_1 10_2 14_1
 
 clean:
 	rm -rf target/release/*
@@ -14,6 +14,7 @@ clean:
 6_2: rust_6_2
 10_1: rust_10_1
 10_2: rust_10_2
+14_1: rust_14_1
 
 python_1_1: src/1_1.py var/1.txt
 	cat var/1.txt | python3 src/1_1.py
@@ -51,9 +52,12 @@ rust_10_1: target/release/10_1 var/10.txt src/10_1.rs
 rust_10_2: target/release/10_2 var/10.txt src/10_2.rs
 	cat var/10.txt | target/release/10_2
 
-$(patsubst src/%.rs, target/release/%, $(wildcard src/*.rs)): $(wildcard src/%.rs) Cargo.toml
+rust_14_1: target/release/14_1 var/14.txt src/14_1.rs
+	cat var/14.txt | target/release/14_1
+
+$(patsubst src/%.rs, target/release/%, $(wildcard src/*.rs)): $(wildcard src/%.rs)
 	cargo build --release
 
-.PHONY: all 1_1 1_2 2_1 3_1 4_1 5_1 5_2 6_1 6_2 10_1 10_2 \
+.PHONY: all 1_1 1_2 2_1 3_1 4_1 5_1 5_2 6_1 6_2 10_1 10_2 14_1 \
 	python_1_1 python_2_1 python_3_1 python_4_1 \
-	rust_1_1 rust_1_2 rust_5_1 rust_5_2 rust_6_1 rust_6_2 rust_10_1 rust_10_2
+	rust_1_1 rust_1_2 rust_5_1 rust_5_2 rust_6_1 rust_6_2 rust_10_1 rust_10_2 rust_14_1
---
+++ src/14_1.rs
@@ -0,0 +1,52 @@
+extern crate regex;
+
+use regex::Regex;
+
+struct Reindeer {
+    pub name: String,
+    pub speed: i64,
+    pub fly_interval: i64,
+    pub rest_interval: i64,
+}
+
+struct Parser {
+    regex: Regex,
+}
+
+fn main() {
+    use std::io::{stdin, BufRead};
+    let input = stdin();
+    let parser = Parser::new();
+    let max_distance = input.lock().lines()
+        .map(|line| parser.parse(&line.unwrap()[..]) )
+        .map(|reindeer| get_distance(&reindeer, 2503))
+        .max().unwrap();
+    println!("{}", max_distance);
+}
+
+impl Parser {
+    pub fn new() -> Parser {
+        Parser {
+        regex: Regex::new(
+                r"^(?P<name>\w+) can fly (?P<speed>\d+) km/s for (?P<fly_interval>\d+) seconds, but then must rest for (?P<rest_interval>\d+) seconds.$"
+            ).unwrap()
+        }
+    }
+
+    pub fn parse(&self, line: &str) -> Reindeer {
+        let captures = self.regex.captures(line).unwrap();
+        Reindeer {
+            name: captures.name("name").unwrap().to_string(),
+            speed: captures.name("speed").unwrap().parse::<i64>().unwrap(),
+            fly_interval: captures.name("fly_interval").unwrap().parse::<i64>().unwrap(),
+            rest_interval: captures.name("rest_interval").unwrap().parse::<i64>().unwrap(),
+        }
+    }
+}
+
+fn get_distance(reindeer: &Reindeer, interval: i64) -> i64 {
+    use std::cmp::min;
+    let full = interval / (reindeer.fly_interval + reindeer.rest_interval);
+    let last = interval % (reindeer.fly_interval + reindeer.rest_interval);
+    (full * reindeer.fly_interval + min(last, reindeer.fly_interval)) * reindeer.speed
+}
